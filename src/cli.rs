06febd79c17c927c2a34b0fb77c5e12c18ce0d7e
atbraz <antoniomtbraz@gmail.com>
2024-04-28 18:13:42 -0300

    feat: add output to pyproject.toml

--- src/cli.rs
+++ src/cli.rs
@@ -12,6 +12,7 @@ use std::path::PathBuf;
 pub struct Args {
     #[clap(
         value_name = "PATH",
+        default_value = ".",
         help = "Input path, can be a file or a directory",
         long_help = "Input path, can be a file or a directory. If a directory is provided, all Python files in the directory will be parsed for imports.",
         required = true,
@@ -21,18 +22,29 @@ pub struct Args {
 
     #[clap(
         value_name = "OUTPUT",
-        default_value = "requirements.txt",
-        short,
-        long,
+        default_value = "./requirements.txt",
+        short = 'o',
+        long = "output",
         help = "Output file to use",
         long_help = "Output file to use. If not provided, will write to requirements.txt in the current directory. If set to 'pyproject.toml', will write to project.dependencies (must be in current directory).",
         value_parser = clap::value_parser!(PathBuf)
     )]
-    pub output: Option<PathBuf>,
+    pub output: PathBuf,
+
+    #[clap(
+        value_name = "TREE",
+        default_value = "false",
+        short = 't',
+        long = "tree",
+        help = "Output a tree of dependencies",
+        long_help = "Output a tree of dependencies. If set to 'true', will output a tree of dependencies to the output file.",
+        action = ArgAction::SetTrue,
+    )]
+    pub tree: bool,
 
     #[clap(
         value_name = "DRY_RUN",
-        short,
+        default_value = "false",
         long,
         help = "Dry run, do not write to file",
         long_help = "Simulates what would be written to the output file should in a regular call.",
@@ -42,8 +54,8 @@ pub struct Args {
 
     #[clap(
         value_name = "VERBOSE",
-        short,
-        long,
+        short = 'v',
+        long = "verbose",
         help = "Verbose mode",
         action = ArgAction::SetTrue,
     )]
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
 use clap::Parser;
+
 mod cli;
-mod input;
-mod map;
-mod parse;
+mod imports;
 mod utils;
-mod write;
+mod writer;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli = cli::Args::parse();
 
-    let imports = input::process_path(&cli.path)?;
-    let packages = map::imports_to_packages(&imports);
-    let versions = map::packages_to_versions(&packages);
-    match write::to_requirements(versions) {
+    let imports = imports::reader::process_path(&cli.path)?;
+    let packages = imports::mapper::imports_to_packages(&imports);
+    let versions = imports::mapper::packages_to_versions(&packages);
+    match writer::to_output(&versions, &cli.output) {
         Ok(_) => println!("Successfully wrote requirements.txt"),
         Err(e) => eprintln!("Error writing requirements.txt: {}", e),
     }
